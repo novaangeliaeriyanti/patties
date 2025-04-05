a5a890c902e3e3c0e467e088738b1838276b7a9c
novaangeliaeriyanti <angelianova1998@gmail.com>
2025-04-06 00:13:05 +0700

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Advantages2 />
       <Advantages3 />
       <Tagline />     
-      <Flavors />
+      {/* <Flavors /> */}
       <Footer />
     </>
   );
